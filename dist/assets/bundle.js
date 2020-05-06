7734202960902516efc804b8f67cf6a6aa7aa912
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-06 15:08:15 +0545

    Add NPM scripts watch tag

---
+++ dist/assets/bundle.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var greet = function greet(name) {
+  console.log("hello ".concat(name));
+};
+
+greet('viv');
+greet('viv-bhai');
+greet('viveshCodes');
+greet('vivesh');
+greet('sanyogita');
+
+var User = function User() {
+  _classCallCheck(this, User);
+
+  this.score = 0;
+};
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const greet = (name) =>{
+    console.log(`hello ${name}`);
+};
+
+greet('viv');
+greet('viv-bhai');
+greet('viveshCodes');
+greet('vivesh');
+greet('sanyogita');
+
+class User{
+    constructor(){
+        this.score=0;
+    }
+}
