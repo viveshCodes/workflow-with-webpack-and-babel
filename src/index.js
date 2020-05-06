4eb1ac082167ab5fe6784b9ee8c23fe6d402c529
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-06 20:30:58 +0545

    Add default exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 // import './dom';
 import {styleBody , addTitle, greeting} from './dom'; // Not only function but dom.js will also be loaded
+import users, {getPremiumUsers} from './data';   // here, 'users' can be named with any name
 
 console.log('index file');
 addTitle('test export');
 
 console.log(greeting);
+
+console.log(users);
+
+const premUsers = getPremiumUsers(users);
+console.log(premUsers);
