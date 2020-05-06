84cebf794573a826494279645a7a769620e6b196
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-06 16:32:54 +0545

    Add webpack file

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// This doesn't run in  browser .
+// It only runs on system with the help of nodejs
+
+const path = requires('path');  // From node library
+module.exports = {
+    entry:'./src/index.js',
+    output:{
+        path:path.resolve(__dirname, 'dist/assets'),
+        filename:'bundle.js'
+    }
+};  
